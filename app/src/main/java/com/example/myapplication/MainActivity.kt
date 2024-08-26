302dcd563c791b79d8b98d879a1c95bb4f37504d
Sergio21678 <sergio.vilca@tecsup.edu.pe>
2024-08-26 17:05:36 -0500

    Añadiendo OutlinedTextField al proyecto

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -48,6 +52,13 @@ fun MyLazyColumn() {
     }
 }
 
+@Composable
+fun MyFloatingActionButton() {
+    FloatingActionButton(onClick = { /* Do something */ }) {
+        Icon(Icons.Filled.Add, contentDescription = "Add")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -55,3 +66,4 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
