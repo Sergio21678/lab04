1b3db37555774dee93e94481b05bddb57c23d516
Sergio21678 <sergio.vilca@tecsup.edu.pe>
2024-08-26 16:29:15 -0500

    Añadiendo LazyColumn al proyecto

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,6 +37,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun MyLazyColumn() {
+    LazyColumn {
+        items(10) { index ->
+            Text(text = "Item #$index")
+        }
+    }
 }
 
 @Preview(showBackground = true)
