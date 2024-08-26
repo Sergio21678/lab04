7e24f14e14859da6d453a3b78188ef63bb541c7f
Sergio21678 <sergio.vilca@tecsup.edu.pe>
2024-08-26 17:38:34 -0500

    Modificaciones de estilo al primer componente en Upgrading-component-1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -28,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -57,9 +59,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MyLazyColumn() {
-    LazyColumn {
+    LazyColumn(
+        contentPadding = PaddingValues(vertical = 8.dp)
+    ) {
         items(10) { index ->
-            Text(text = "Item #$index")
+            Text(
+                text = "Item #$index",
+                modifier = Modifier.padding(8.dp) // Espaciado alrededor de cada elemento
+            )
         }
     }
 }
